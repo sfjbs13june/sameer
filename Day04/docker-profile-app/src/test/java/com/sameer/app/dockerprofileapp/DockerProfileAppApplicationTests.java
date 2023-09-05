2550cc776a205ef3ec7f043f4d899811e7dd5b1d
sfjbs <sameer.ranade@tcs.com>
2023-09-05 07:44:20 +0000

    Git profileapp creation

---
+++ Day04/docker-profile-app/src/test/java/com/sameer/app/dockerprofileapp/DockerProfileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.app.dockerprofileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerProfileAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
